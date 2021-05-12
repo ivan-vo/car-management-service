908b50624125ca4f6e26db3194c91ab9f1d4216e
Ivan Voronov <ivanvo@tahometer.com>
2021-05-12 15:23:11 +0300

    add action load Manager

--- management-app/src/store/actions/deals-actions.js
+++ management-app/src/store/actions/deals-actions.js
@@ -1,1 +1,7 @@
-export const { MANAGERS_LOADED } = 'managers/loaded'
+export const { MANAGERS_LOADED } = 'managers/loaded';
+
+export const loadManagers = () => async (dispatch) => {
+    fetch('http://localhost:5000/managers')
+        .then(res => res.json())
+        .then(managers => dispatch({type: MANAGERS_LOADED, payload: managers}))
+}
