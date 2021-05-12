d9f996811b9a6a7cf0fece5101e7c2e3cbf3cdda
Ivan Voronov <ivanvo@tahometer.com>
2021-05-12 16:33:35 +0300

    hotfix

--- management-app/src/store/actions/deals-actions.js
+++ management-app/src/store/actions/deals-actions.js
@@ -2,7 +2,7 @@ export const MANAGERS_LOADED = 'managers/loaded';
 export const CARS_LOADED = 'cars/loaded';
 
 export const loadManagers = () => async (dispatch) => {
-    fetch('http://localhost:5000/carrs')
+    fetch('http://localhost:5000/cars')
         .then(res => res.json())
         .then(cars => dispatch({
             type: MANAGERS_LOADED,
