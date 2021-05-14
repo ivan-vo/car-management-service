a7a073b7bdf1f47ac0953044a6efd0f4a365d508
Ivan Voronov <ivanvo@tahometer.com>
2021-05-14 18:14:40 +0300

    create new DTO

---
+++ managementAPI/DTO/ManagerDealsCountDTO.cs
@@ -0,0 +1,8 @@
+namespace managementAPI
+{
+    public class ManagerDealsCountDTO
+    {
+        public int count { get; set; }
+        public int managerId { get; set; }
+    }
+}
---
+++ managementAPI/DTO/TopManagerDTO.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace managementAPI
+{
+    public class TopManagerDTO
+    {
+        public Manager topManager { get; set; }
+        public int countDeals { get; set; }
+    }
+}
