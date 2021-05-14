7092dcaafa3dcd71a7810769da5f408597bbd948
Ivan Voronov <ivanvo@tahometer.com>
2021-05-14 18:14:04 +0300

    remuve not using importing

--- managementAPI/DTO/DealDateManagerCarDTO.cs
+++ managementAPI/DTO/DealDateManagerCarDTO.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json;
 
 namespace managementAPI
 {
